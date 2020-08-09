4f355e4da537031c712fad9476acfe1d60fb6973
Dante Marinho <dantecnia@hotmail.com>
2020-08-09 15:44:44 +0100

    Starting Redux-Simples project (initial practical concepts about Redux).

---
+++ redux-simples/src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Card from './components/Card';
+import Intervalo from './components/Intervalo'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Exercício React-Redux (Simples)</h1>
+      <div className="linha">
+        <Intervalo />
+      </div>
+      <div className="linha">
+        <Card title="Card 2" green>X</Card>
+        <Card title="Card 3" blue>Y</Card>
+        <Card title="Card 4" purple>Y</Card>
+      </div>
+    </div>
+  );
+}
+
+export default App;
