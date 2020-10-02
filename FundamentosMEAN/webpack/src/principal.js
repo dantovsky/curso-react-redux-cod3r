6b6e8713cff98db13da862c454cbe3596d20fcc9
Dante Marinho <dantecnia@hotmail.com>
2020-10-02 01:14:45 +0100

    Saving jobs, finishing Webpack.

--- FundamentosMEAN/webpack/src/principal.js
+++ FundamentosMEAN/webpack/src/principal.js
@@ -1,6 +1,6 @@
 // const Pessoa = require('./pessoa') // Sistema de modules do CommonsJS
 import Pessoa from './pessoa' // Forma de importar padrão do ECMAScript
-import './assets/css/estilo.css'
+import './assets'
 
 const atendente = new Pessoa
 console.log(atendente.cumprimentar())
--- FundamentosMEAN/webpack/webpack.config.js
+++ FundamentosMEAN/webpack/webpack.config.js
@@ -1,12 +1,24 @@
+const modoDev = process.env.NODE_ENV !== 'production'
 const webpack = require('webpack')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
 
 module.exports = {
-    mode: 'development',
+    mode: modoDev ? 'development' : 'production',
     entry: './src/principal.js',
     output: {
         filename: 'principals.js',
         path: __dirname + '/public'
+    },
+    optimization: {
+        minimizer: [
+            new UglifyJsPlugin({
+                cache: true,
+                parallel: true
+            }),
+            new OptimizeCSSAssetsPlugin({})
+        ]
     },
     plugins: [
         new MiniCssExtractPlugin({
@@ -15,12 +27,16 @@ module.exports = {
     ],
     module: {
         rules: [{
-            test: /\.css$/,
+            test: /\.s?[ac]ss$/, // Encontra arquivos .css, .scss, .sass // Somente para encontrar CSS: /\.css$/,
             use: [
                 MiniCssExtractPlugin.loader, // extrair o CSS para um arquivo específico (ele é conflitante com o 'style-loader')
                 // 'style-loader', // Adiciona CSS a DOM injetando a tag <style>
-                'css-loader' // interpreta @import, url()...
+                'css-loader', // interpreta @import, url()...
+                'sass-loader',
             ]
+        }, {
+            test: /\.(png|svg|jpg|gif)$/,
+            use: ['file-loader']
         }]
     }
 }
