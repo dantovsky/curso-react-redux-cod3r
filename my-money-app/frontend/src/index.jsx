eed47a55abf0a415a987c630652f2a14204add44
Dante Marinho <dantecnia@hotmail.com>
2020-12-12 20:16:36 +0000

    My-Money-App: basic redux configuration.

--- my-money-app/frontend/src/index.jsx
+++ my-money-app/frontend/src/index.jsx
@@ -1,5 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './main/app'
+import React from "react";
+import ReactDOM from "react-dom";
+import { createStore } from "redux"; // for Redux
+import { Provider } from "react-redux"; // for Redux (envolve a tag App)
 
-ReactDOM.render(<App />, document.getElementById('app'))
+import App from "./main/app";
+import reducers from "./main/reducers"; // for Redux
+
+// Definicao da store, passando os reducers para o método createStore()
+const store = createStore(reducers); // for Redux
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("app")
+);
---
+++ my-money-app/frontend/src/main/reducers.js
@@ -0,0 +1,20 @@
+import { combineReducers } from 'redux'
+
+/**
+ * Este módulo combina todos os Reducers da app
+ */
+
+//  Resultado de todos os Reducers da app
+const rootReducers = combineReducers({
+    // Referencia para a store, chamada "dashboard", que aponta para um método reducer (uma função pura)
+    // Reducer :: recebe dois params
+    // 1. o próprio estado (a store)
+    // 2. a ação (action que foi disparada para que o reducer fosse chamado) 
+    dashboard: () => ({ summary: { credito: 100, debt: 50 } })
+})
+
+export default rootReducers
+
+// Este é o arquivo que faz o papel de concatenar todos os reducers da app.
+// Os dados dentro do Redux e gerenciado a partir de um objeto único, que é justamente
+// utilizando este método que combina todos os reducers.
